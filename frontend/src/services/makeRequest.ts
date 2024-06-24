315d3ace8899ef3a81148a99ed29c595431eeb25
EshaanAgg <96648934+EshaanAgg@users.noreply.github.com>
2024-06-24 07:41:46 +0530

    mock dev responses

--- frontend/src/services/makeRequest.ts
+++ frontend/src/services/makeRequest.ts
@@ -18,11 +18,28 @@ export const makeAPIRequest = async (
     });
   }
 
-  if (fileContent) {
-    const imgBase64 = fileContent.replace(/^data:image\/[a-z]+;base64,/, '');
-    return axios.post<ResponseData>(import.meta.env.VITE_BACKEND_API_URL + '/postImage', {
-      base64img: imgBase64,
-      k
+  if (fileContent || imageFile) {
+    // const imgBase64 = fileContent.replace(/^data:image\/[a-z]+;base64,/, '');
+    // return axios.post<ResponseData>(import.meta.env.VITE_BACKEND_API_URL + '/postImage', {
+    //   base64img: imgBase64,
+    //   k
+    // });
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({
+          data: {
+            images: [
+              'https://m.media-amazon.com/images/I/512WTE69iuL._AC_SX342_.jpg',
+              'https://m.media-amazon.com/images/I/81-tEKRkNlL._AC_SX342_.jpg',
+              'https://m.media-amazon.com/images/I/716r02fA7DL._AC_SX342_.jpg',
+              'https://m.media-amazon.com/images/I/61pa3kh30PL._AC_SY445_.jpg',
+              'https://m.media-amazon.com/images/I/51Xkme6x34L._AC_UL320_.jpg',
+              'https://m.media-amazon.com/images/I/51QZxiYrXaL._AC_UL320_.jpg',
+              'https://m.media-amazon.com/images/I/51uCdvvXriL._AC_SY445_.jpg'
+            ].slice(0, k)
+          }
+        });
+      }, 2000);
     });
   }
 
--- frontend/src/services/makeTextRequest.ts
+++ frontend/src/services/makeTextRequest.ts
@@ -4,9 +4,35 @@ type ResponseData = {
   images: string[];
 };
 
-export const makeTextRequest = async (queryText: string, k: number) => {
-  return axios.post<ResponseData>(import.meta.env.VITE_TEXT_BACKEND_API_URL + '/postText', {
-    searchString: queryText,
-    k
+const images = [
+  'https://m.media-amazon.com/images/I/71Fot8BNhUL._AC_SY445_.jpg',
+  'https://m.media-amazon.com/images/I/71HJuntkSXL._AC_SY445_.jpg',
+  'https://m.media-amazon.com/images/I/813qJVEH5mL._AC_SY445_.jpg',
+  'https://m.media-amazon.com/images/I/81MIUIz6LiL._MCnd_AC_UL320_.jpg',
+  'https://m.media-amazon.com/images/I/711meGLRdCL._MCnd_AC_UL320_.jpg',
+  'https://m.media-amazon.com/images/I/61zle4VaOjL._AC_SX342_.jpg',
+  'https://m.media-amazon.com/images/I/81gN0UpIXPL._AC_SY445_.jpg',
+  'https://m.media-amazon.com/images/I/71S9W1b1ojL._AC_SY445_.jpg'
+];
+
+export const makeTextRequest = async (
+  queryText: string,
+  k: number
+): Promise<{
+  data: ResponseData;
+}> => {
+  // return axios.post<ResponseData>(import.meta.env.VITE_TEXT_BACKEND_API_URL + '/postText', {
+  //   searchString: queryText,
+  //   k
+  // });
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data: {
+          images: images.slice(0, k)
+        }
+      });
+    }, 2000);
   });
 };
